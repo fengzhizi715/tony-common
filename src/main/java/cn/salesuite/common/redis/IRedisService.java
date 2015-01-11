5fd020b844bd431564058122bead18e127b1246c
Tony Shen <fengzhizi715@126.com>
2015-01-11 08:25:44 -0600

    增加guava cache的模板类，redis的封装

---
+++ src/main/java/cn/salesuite/common/redis/IRedisService.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package cn.salesuite.common.redis;
+
+/**
+ * @author Tony Shen
+ *
+ */
+public interface IRedisService<K, V> {
+	
+    /**
+     * if expiredTime <=0 then never timeout
+     * @param key
+     * @param value
+     * @param expiredTime
+     */
+    public void set(K key, V value, long expiredTime);
+ 
+    public V get(K key);
+ 
+    public void del(K key);
+}
