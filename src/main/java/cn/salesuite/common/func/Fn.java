a700e6c4a0dd48ab8a060cd6fdea92cb656cd362
Tony Shen <fengzhizi715@126.com>
2016-12-24 22:59:19 +0800

    update func and reflect

---
+++ src/main/java/cn/salesuite/common/func/Fn.java
@@ -0,0 +1,63 @@
+package cn.salesuite.common.func;
+
+import cn.salesuite.common.func.functions.Action1;
+import cn.salesuite.common.func.functions.Func1;
+import cn.salesuite.common.func.functions.Predicate;
+import cn.salesuite.common.utils.Preconditions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Tony Shen on 16/9/9.
+ */
+
+public class Fn {
+
+    public static <T, R> void forEach(Action1<? super T> func, Iterable<T> iterable) {
+
+        if (Preconditions.isNotBlank(iterable)) {
+            for (T t : iterable) {
+                func.call(t);
+            }
+        }
+    }
+
+    public static <T, R> List<R> map(Func1<? super T, ? extends R> func, List<T> list) {
+        List<R> r = new ArrayList<R>();
+        if (Preconditions.isNotBlank(list)) {
+            for (T l : list) {
+                r.add(func.call(l));
+            }
+        }
+
+        return r;
+    }
+
+    public static <T> T first(Predicate<? super T> p,
+                              List<? extends T> list) {
+
+        if (Preconditions.isNotBlank(list)) {
+            for (T t : list) {
+                if (p.accept(t)) {
+                    return t;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public static <T, R>void first(final Func1<? super T, ? extends R> func, Predicate<? super T> p,
+                                   List<? extends T> list) {
+
+        if (Preconditions.isNotBlank(list)) {
+            for (T t : list) {
+                if (p.accept(t)) {
+                    func.call(t);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/cn/salesuite/common/reflect/NULL.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package cn.salesuite.common.reflect;
+
+/**
+ * @author Tony Shen
+ *
+ */
+public class NULL {
+
+}
