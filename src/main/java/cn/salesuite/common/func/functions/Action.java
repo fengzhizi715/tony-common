5e45991c3675958a8879e41f879cd1b985528f30
Tony Shen <fengzhizi715@126.com>
2017-01-12 07:52:44 -0600

    删除func模块

--- src/main/java/cn/salesuite/common/func/functions/Action.java
+++
@@ -1,7 +0,0 @@
-package cn.salesuite.common.func.functions;
-
-/**
- * Created by Tony Shen on 16/9/9.
- */
-interface Action {
-}
--- src/main/java/cn/salesuite/common/func/functions/Predicate.java
+++
@@ -1,9 +0,0 @@
-package cn.salesuite.common.func.functions;
-
-/**
- * Created by Tony Shen on 16/9/9.
- */
-public interface Predicate<T> {
-
-    boolean accept(T t);
-}
